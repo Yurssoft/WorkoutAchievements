8474a89c62dda575f21ff9aae23f873b6c9ba649
Yurii Boiko <Yurssoft@users.noreply.github.com>
2023-08-01 15:26:23 -0700

    Initial commit

---
+++ WorkoutAchievements/WorkoutAchievementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutAchievementsApp.swift
+//  WorkoutAchievements
+//
+//  Created by Yurii B on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutAchievementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
