e95a7e365b0209c24d0de4b5843a0cef23c4071b
Yurii Boiko <Yurssoft@users.noreply.github.com>
2023-08-08 20:00:01 -0700

    Update Mock.swift

--- WorkoutsClient/Sources/WorkoutsClient/Mock.swift
+++ WorkoutsClient/Sources/WorkoutsClient/Mock.swift
@@ -4,7 +4,7 @@ extension WorkoutsClient {
     public static let authorizedToReadMock = Self { _ in
         [Workout(calories: "1010"), Workout(calories: "1010")]
     } isAuthorizedToUse: {
-        true
+        .sharingAuthorized
     } requestReadAuthorization: {
         
     }
