7b96b084941a38d3dfa02989e42406d0f5b81289
Yurii Boiko <Yurssoft@users.noreply.github.com>
2023-08-09 14:54:44 -0700

    Add state

--- WorkoutAchievements/AchievementsView.swift
+++ WorkoutAchievements/AchievementsView.swift
@@ -10,13 +10,46 @@ import WorkoutsViewFeature
 import WorkoutsClient
 import WorkoutsClientLive
 
+extension AchievementsView {
+    enum ViewState {
+        case initialized
+        case showList
+        case error
+    }
+}
+
 struct AchievementsView: View {
     let workoutsClient: WorkoutsClient
+    @State private var state = ViewState.initialized
     var body: some View {
         VStack {
-            WorkoutsView(client: workoutsClient)
+            switch state {
+            case .initialized:
+                Button("Request Authorization & Load List") {
+                    checkAuthorizationStatusAndLoadList()
+                }
+                
+            case .showList:
+                WorkoutsView(client: workoutsClient)
+                
+            case .error:
+                Text("Error")
+            }
         }
         .padding()
+    }
+}
+
+private extension AchievementsView {
+    func checkAuthorizationStatusAndLoadList() {
+        Task {
+            do {
+                try await workoutsClient.requestReadAuthorization()
+                state = .showList
+            } catch {
+                state = .error
+            }
+        }
     }
 }
 
