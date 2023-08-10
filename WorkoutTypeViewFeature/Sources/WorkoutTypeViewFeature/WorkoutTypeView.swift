0fbc6e35aac7d9bf11b9cb0a862c50c6a4730ff0
Yurii Boiko <Yurssoft@users.noreply.github.com>
2023-08-10 15:44:31 -0700

    Add toggle

--- WorkoutTypeViewFeature/Sources/WorkoutTypeViewFeature/WorkoutTypeView.swift
+++ WorkoutTypeViewFeature/Sources/WorkoutTypeViewFeature/WorkoutTypeView.swift
@@ -63,6 +63,8 @@ public struct WorkoutTypeView: View {
                 }
                 .pickerStyle(.menu)
                 
+                Toggle("Is Ascending", isOn: $selectedQuery.isAscending)
+                
                 Text("Selected Query:\n \(selectedQuery.measurmentType.name)\n \(selectedQuery.workoutType.name)")
                     .multilineTextAlignment(.center)
             }
