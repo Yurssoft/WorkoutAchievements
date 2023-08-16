3204f27f3b6f00cb61f59d443f3a0f8dfef0f8a6
Yurii Boiko <Yurssoft@users.noreply.github.com>
2023-08-15 19:07:31 -0700

    Merge all libraries to single SPM

---
+++ AppDependencies/Package.swift
@@ -0,0 +1,50 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "AppDependencies",
+    platforms: [.iOS(.v17), .watchOS(.v10)],
+    products: [
+        .library(
+            name: "AchievementsViewFeature",
+            targets: ["AchievementsViewFeature"]),
+        .library(
+            name: "RequestPermissionsViewFeature",
+            targets: ["RequestPermissionsViewFeature"]),
+        .library(
+            name: "WorkoutTypeViewFeature",
+            targets: ["WorkoutTypeViewFeature"]),
+        .library(
+            name: "WorkoutsViewFeature",
+            targets: ["WorkoutsViewFeature"]),
+        .library(
+            name: "WorkoutsClient",
+            targets: ["WorkoutsClient"]),
+        .library(
+            name: "WorkoutsClientLive",
+            targets: ["WorkoutsClientLive"]),
+    ],
+    targets: [
+        .target(
+            name: "AchievementsViewFeature",
+            dependencies: ["WorkoutsClient",
+                           "WorkoutsViewFeature",
+                           "RequestPermissionsViewFeature",
+                           "WorkoutTypeViewFeature"]),
+        .target(
+            name: "RequestPermissionsViewFeature",
+            dependencies: ["WorkoutsClient", "WorkoutsViewFeature"]),
+        .target(
+            name: "WorkoutTypeViewFeature",
+            dependencies: ["WorkoutsClient"]),
+        .target(
+            name: "WorkoutsViewFeature",
+            dependencies: ["WorkoutsClient"]),
+        .target(
+            name: "WorkoutsClient"),
+        .target(
+            name: "WorkoutsClientLive",
+            dependencies: ["WorkoutsClient"]),
+    ]
+)
