587045f2b81cfdd9dece7394e3118abd4ada4576
Yurii Boiko <Yurssoft@users.noreply.github.com>
2023-08-21 15:24:06 -0700

    Add placeholder app for trusting account

---
+++ PCT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PCT
+//
+//  Created by Yurii B on 8/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PCT/PCTApp.swift
@@ -0,0 +1,17 @@
+//
+//  PCTApp.swift
+//  PCT
+//
+//  Created by Yurii B on 8/21/23.
+//
+
+import SwiftUI
+
+@main
+struct PCTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
