1bfc6f3498facdc15a292818b337ecfb3ff033a0
Yurii Boiko <Yurssoft@users.noreply.github.com>
2023-12-10 12:47:05 -0800

    Update InformationView.swift

--- AppDependencies/Sources/InformationView/InformationView.swift
+++ AppDependencies/Sources/InformationView/InformationView.swift
@@ -16,13 +16,21 @@ public struct InformationView: View {
     
     public var body: some View {
         VStack {
-            Image(systemName: "info")
-                .imageScale(.large)
-                .tint(.blue)
-            Link("Feedback & Contact", destination: URL(string: "https://www.linkedin.com/in/yurii-boiko/")!)
-                .padding()
-            Spacer()
+            VStack {
+                Image(systemName: "info")
+                    .imageScale(.large)
+                    .tint(.blue)
+                Link("Feedback & Contact", destination: URL(string: "https://www.linkedin.com/in/yurii-boiko/")!)
+                    .padding()
+            }
             
+            VStack {
+                Text("About")
+                    .font(.headline)
+                Text("When using workouts on watch, like running or swimming, have you ever wondered about your best workout?\nWorkout app allows to see your best day/week/month/year of exercise. Easily find your best hike ever. See averages for selected period.")
+                    .padding()
+            }
+            Spacer()
         }
         .toolbar {
             ToolbarItem(placement: .automatic) {
